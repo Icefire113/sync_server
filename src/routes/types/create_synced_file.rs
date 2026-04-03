aac190c5e8d07a218b811a6ffdd06c502eae43a3
Icefire113 <phelan.gormley@gmail.com>
2026-04-02 22:46:17 -0700

    More API definitions

---
+++ src/routes/types/create_synced_file.rs
@@ -0,0 +1,29 @@
+use std::{collections::HashMap, path::PathBuf};
+
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct CreateDiscrimReq {
+    pub username: String,
+    pub file: SyncedFile,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct SyncedFile {
+    pub id: Uuid,
+    pub file_path_per_machine: HashMap<String, PathBuf>,
+    pub custom_name: Option<String>,
+    pub file_state: FileState,
+    pub file_blob: Vec<u8>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct FileState {
+    pub hash: u64,
+    pub file_last_modified: DateTime<Utc>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct CreateDiscrimRes {}
